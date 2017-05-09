40538d150fdd497deb603c7eeec1580c0032bba7
Felipe Mietto <mietto.ti@gmail.com>
2017-05-09 01:03:55 -0300

    css parcial

--- app/views/currency-quotes/currency-quotes.js
+++ app/views/currency-quotes/currency-quotes.js
@@ -35,7 +35,7 @@ angular.module('myApp.currency-quotes', ['ngRoute', 'myApp.quote', 'myApp.newsle
   };
 
   setTimeout(this.refreshQuotes, 100);
-  this.refreshInterval = setInterval(this.refreshQuotes, 10 * 1000);
+  //this.refreshInterval = setInterval(this.refreshQuotes, 10 * 1000);
 
   this.register = function () {
     var self = this;
@@ -54,5 +54,27 @@ angular.module('myApp.currency-quotes', ['ngRoute', 'myApp.quote', 'myApp.newsle
         break;
     }
   };
+
+  this.formatCurrency = function(number) {
+    var num = this.formatNumber(number.toString());
+    return parseFloat(num).toFixed(2).toString().replace(".", ",");
+  }
+
+  this.formatInt = function(number) {
+    var num = this.formatNumber(number.toString());
+    return num.split(".")[0].replace(",", ".");;
+  }
+
+  this.formatNumber = function (nStr) {
+      nStr += '';
+      var x = nStr.split('.');
+      var x1 = x[0];
+      var x2 = x.length > 1 ? '.' + x[1] : '';
+      var rgx = /(\d+)(\d{3})/;
+      while (rgx.test(x1)) {
+              x1 = x1.replace(rgx, '$1' + ',' + '$2');
+      }
+      return x1 + x2;
+  }
 
 }])
